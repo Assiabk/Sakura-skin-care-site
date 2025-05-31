71e324258f780d25cce9a4aaf68da69b7a295885
Assia <you@example.com>
2025-05-31 19:08:34 +0100

    Update site content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./frontend/components/Navbar";
 import Home from "./frontend/components/Home";  
 import New from "./frontend/components/New";
